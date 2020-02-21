88aa2bb4ff5772a58085d4b1152b04d29409aaef
W4mmy <wammy3224@gmail.com>
2020-02-20 21:44:57 -0700

    v 1.03
    Created the blog page css and set up a parent navigation to ensure correct placement within the programs.

--- JS/mainPageJS.js
+++ JS/mainPageJS.js
@@ -1,3 +1,15 @@
+function goHome() {
+    window.location.href = "index"
+}
+
 function goBlog() {
     window.location.href = "Blog/blogs";
+}
+
+function paren_goHome() {
+    window.location.href = "index"
+}
+
+function parent_goBlog() {
+    window.location.href = "../Blog/blogs"
 }
