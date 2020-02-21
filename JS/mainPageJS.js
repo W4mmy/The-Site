40443bfe0c1e3f067840193840ab47cdeb149b90
W4mmy <wammy3224@gmail.com>
2020-02-21 15:21:01 -0700

    v 1.08
    Major Changes, Optimized for mobile with a dropdown bar for easier navigation throughout the site

--- JS/mainPageJS.js
+++ JS/mainPageJS.js
@@ -13,3 +13,23 @@ function parent_goHome() {
 function parent_goBlog() {
     window.location.href = "../Blog/blogs"
 }
+
+    
+    
+function display() {
+    var navBar = document.getElementById("nav_Bar")
+    if (navBar.style.display !== "block") {
+        navBar.style.display = "block";
+    } else {
+        navBar.style.display = "none";
+    }
+}
+
+function rotate() {
+    var dropdown = document.getElementById("dropdown_triangle")
+    if (dropdown.style.transform !== "rotate(180deg)") {
+        dropdown.style.transform = 'rotate(180deg)'
+    } else {
+        dropdown.style.transform = 'rotate(0deg)'
+    }
+}
